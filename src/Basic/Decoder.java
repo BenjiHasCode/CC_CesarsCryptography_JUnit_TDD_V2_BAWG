b342a837d7d559310359b52e1a5ecf653fc1d341
BenjiHasCode <57215161+BenjiHasCode@users.noreply.github.com>
2020-09-24 19:22:29 +0200

    Changed encryption/decryption to a line to line approach

--- src/Basic/Decoder.java
+++ src/Basic/Decoder.java
@@ -6,11 +6,13 @@ public class Decoder {
     public static String decode(String text){
         String decodedText = "";
         int decodeValue = 0;
+
         Scanner scanner = new Scanner(text);
+
         while(scanner.hasNext()){
             String word = scanner.next();
             if(word.length() == 3){
-                for (int i = 15; i > 0; i--){
+                for (int i = 10; i > 0; i--){
                     String temp = "";
                     for(int j = 0; j < word.length(); j++){
                         temp += (char)(word.charAt(j) - i);
@@ -23,15 +25,21 @@ public class Decoder {
             }
         }
 
-        for(int i = 0; i < text.length(); i++){
-            if (text.charAt(i) != ' '){
-                decodedText += (char)(text.charAt(i) - decodeValue);
-            }else if (text.charAt(i) == ' '){
-                decodedText += " ";
+        Scanner lineScanner = new Scanner(text);
+        while(lineScanner.hasNext()){
+            String line = lineScanner.nextLine();
+
+            for(int i = 0; i < line.length(); i++){
+                if (line.charAt(i) != ' '){
+                    decodedText += (char)(line.charAt(i) - decodeValue);
+                }else if (line.charAt(i) == ' '){
+                    decodedText += " ";
+                }
+                else{
+                    System.out.println("Something went wrong!");
+                }
             }
-            else{
-                System.out.println("Something went wrong!");
-            }
+            decodedText += "\n";
         }
         return decodedText;
     }
--- src/Basic/Encoder.java
+++ src/Basic/Encoder.java
@@ -1,29 +1,30 @@
 package Basic;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Encoder {
     public static String encode(String text){
         //encode text
         Random random = new Random();
-        int randomNumber = random.nextInt(15) + 1;
+        int randomNumber = random.nextInt(10) + 1;
         String encoded = "";
 
-        for(int i = 0; i < text.length(); i++){
-            if (text.charAt(i) != ' '){
-                encoded += (char)(text.charAt(i) + randomNumber);
-            }else if (text.charAt(i) == ' '){
-                encoded += " ";
-            }
-            //how to keep newline characters?
-            /*else if(text.charAt(i) == '\\'){
-                if(text.charAt(i+1) == 'n'){
-                    encoded += "\n";
+        Scanner scanner = new Scanner(text);
+        while(scanner.hasNext()){
+            String line = scanner.nextLine();
+
+            for(int i = 0; i < line.length(); i++){
+                if (line.charAt(i) != ' '){
+                    encoded += (char)(line.charAt(i) + randomNumber);
+                }else if (line.charAt(i) == ' '){
+                    encoded += " ";
                 }
-            }*/
-            else{
-                System.out.println("Something went wrong!");
+                else{
+                    System.out.println("Something went wrong!");
+                }
             }
+            encoded += "\n";
         }
         return encoded;
     }
