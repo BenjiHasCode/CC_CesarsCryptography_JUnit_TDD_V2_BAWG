fdc2c16c19de3e3d11612ad800b2d4c7e531c9d6
BenjiHasCode <57215161+BenjiHasCode@users.noreply.github.com>
2020-09-25 10:39:31 +0200

    Updated test to include a check of original compared to decoded text

--- test/Basic/DecoderTest.java
+++ test/Basic/DecoderTest.java
@@ -2,6 +2,8 @@ package Basic;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DecoderTest {
@@ -11,5 +13,21 @@ class DecoderTest {
         String hello = Encoder.encode("We are the world!");
         hello = Decoder.decode(hello);
         assertEquals("We are the world!", hello);
+    }
+
+    @Test
+    void decodedTest(){
+        String alice = FileHandler.readFile("AliceInWonderland.txt");
+        String encoded = Encoder.encode(alice);
+        String decoded = Decoder.decode(encoded);
+
+        Scanner aScanner = new Scanner(alice);
+        Scanner dScanner = new Scanner(decoded);
+
+        while(aScanner.hasNext()){
+            String original = aScanner.next();
+            String decodedWord = dScanner.next();
+            assertEquals(original, decodedWord);
+        }
     }
 }
